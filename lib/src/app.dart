1ba92ac91e239509b26737c9c5d29cb3b65614d0
Brian Boehm <bri.boehm@gmail.com>
2025-02-09 18:20:30 -0500

    Refactoring Stateless to Stateful to use state.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFFAD1457);
 
+class App extends StatefulWidget{
 
+  @override
+  createState(){
+    return AppState();
+  }
+
+}
 // Create a class that will be our custom widget
 // This class must extend the 'StatelessWidget' base class
-class App extends StatelessWidget{
-
+class AppState extends State<App>{
+  int counter = 0;
   // Define a 'build' method that returns the widgets
   // that 'this' widget must show
   Widget build(context){
